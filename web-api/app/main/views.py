6eae977d4b2b158c4148681b95265bd2feb8b9b8
Cathy Chen <cathychenn@gmail.com>
2016-02-21 14:09:26 -0500

    route signatures

--- web-api/app/main/views.py
+++ web-api/app/main/views.py
@@ -6,4 +6,33 @@ def index():
     return "HELLO"
 
 
+@main.route('/login')
+def login():
+	return
 
+@main.route('/logout')
+def logout():
+	return
+
+@main.route('/signup')
+def signup():
+	return
+
+@main.route('/av_wait')
+def av_wait():
+	return
+
+
+@main.route('/start_time')
+def start_time():
+	return
+
+
+@main.route('/end_time')
+def end_time():
+	return
+
+
+@main.route('/polling_places')
+def polling_places():
+	return
