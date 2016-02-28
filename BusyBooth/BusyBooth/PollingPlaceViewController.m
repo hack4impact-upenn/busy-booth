7ceabb367b103d6de671ea5cbedfbb860d8a783b
sanjayss34 <sanjay_ssubramanian@hotmail.com>
2016-02-28 01:13:20 -0500

    Tested and fixed design issues

--- BusyBooth/BusyBooth/PollingPlaceViewController.m
+++ BusyBooth/BusyBooth/PollingPlaceViewController.m
@@ -38,7 +38,7 @@
     
     NSLog(@"%@", self.parentViewController);
     
-    self.view.backgroundColor = [UIColor whiteColor];
+    self.view.backgroundColor = [UIColor colorWithRed:240.0/256 green:240.0/256 blue:242.0/256 alpha:1.0];
     [self.navigationController.navigationBar
      setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor]}];
     
@@ -59,43 +59,45 @@
     
     UIImageView *pollingPlaceImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Oset_Logo2.png"]];
     [pollingPlaceImage setFrame:CGRectMake(0, 0, 150, 150)];
-    [pollingPlaceImage setCenter:CGPointMake(width/2, height*5/7)];
+    [pollingPlaceImage setCenter:CGPointMake(width/2, height*2/7)];
     [pollingPlaceImage setBackgroundColor:[UIColor clearColor]];
     [self.view addSubview:pollingPlaceImage];
     
     UIFont * customFont = [UIFont fontWithName:@"TimesNewRoman-BoldMT" size:12]; //custom font
     NSString * text = @"Insert Polling Place Address";
     
-    CGSize labelSize = [text sizeWithFont:customFont constrainedToSize:CGSizeMake(380, 20) lineBreakMode:NSLineBreakByTruncatingTail];
-    
-    UILabel *fromLabel = [[UILabel alloc]initWithFrame:CGRectMake(91, 15, labelSize.width, labelSize.height)];
-    fromLabel.text = text;
-    fromLabel.font = customFont;
-    fromLabel.numberOfLines = 1;
-    fromLabel.baselineAdjustment = UIBaselineAdjustmentAlignBaselines; // or UIBaselineAdjustmentAlignCenters, or UIBaselineAdjustmentNone
-    fromLabel.adjustsFontSizeToFitWidth = YES;
-    fromLabel.adjustsLetterSpacingToFitWidth = YES;
-    fromLabel.minimumScaleFactor = 10.0f/12.0f;
-    fromLabel.clipsToBounds = YES;
-    fromLabel.backgroundColor = [UIColor clearColor];
-    fromLabel.textColor = [UIColor blackColor];
-    fromLabel.textAlignment = NSTextAlignmentLeft;
-    [self.view addSubview:fromLabel];
+    UILabel *addressLabel = [[UILabel alloc] initWithFrame:CGRectMake(width/5, height*2.5/7, 200, 200)];
+    addressLabel.text = text;
+    addressLabel.font = customFont;
+    addressLabel.numberOfLines = 1;
+    //addressLabel.baselineAdjustment = UIBaselineAdjustmentAlignBaselines; // or UIBaselineAdjustmentAlignCenters, or UIBaselineAdjustmentNone
+    addressLabel.adjustsFontSizeToFitWidth = YES;
+    addressLabel.adjustsLetterSpacingToFitWidth = YES;
+    addressLabel.minimumScaleFactor = 10.0f/12.0f;
+    addressLabel.clipsToBounds = YES;
+    //addressLabel.backgroundColor = [UIColor clearColor];
+    addressLabel.textColor = [UIColor blackColor];
+    addressLabel.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:addressLabel];
     
     UIButton *viewWaitTimeButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [viewWaitTimeButton setFrame:CGRectMake(0, 0, 50, 30)];
-    [viewWaitTimeButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [viewWaitTimeButton setTitle:@"Back" forState:UIControlStateNormal];
-    [viewWaitTimeButton setCenter:CGPointMake(width/2 - 30, height*5/7)];
+    [viewWaitTimeButton setFrame:CGRectMake(0, 0, 275, 40)];
+    [viewWaitTimeButton setTitleColor: mainColor forState:UIControlStateNormal];
+    [viewWaitTimeButton setBackgroundColor: [UIColor whiteColor]];
+    [viewWaitTimeButton setTitle:@"View Wait Time" forState:UIControlStateNormal];
+    [viewWaitTimeButton setCenter:CGPointMake(width/2, height*6/7)];
     [viewWaitTimeButton addTarget:self action:@selector(presentPollTimes) forControlEvents:UIControlEventTouchUpInside];
+    viewWaitTimeButton.layer.cornerRadius = 8;
     [self.view addSubview:viewWaitTimeButton];
     
     UIButton *viewDrivingDirectionsButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [viewDrivingDirectionsButton setFrame:CGRectMake(0, 0, 50, 30)];
-    [viewDrivingDirectionsButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [viewDrivingDirectionsButton setTitle:@"Back" forState:UIControlStateNormal];
-    [viewDrivingDirectionsButton setCenter:CGPointMake(width/2 - 30, height*5/7 + height)];
+    [viewDrivingDirectionsButton setFrame:CGRectMake(0, 0, 275, 40)];
+    [viewDrivingDirectionsButton setTitleColor: mainColor forState:UIControlStateNormal];
+    [viewDrivingDirectionsButton setBackgroundColor: [UIColor whiteColor]];
+    [viewDrivingDirectionsButton setTitle:@"Get Driving Directions" forState:UIControlStateNormal];
+    [viewDrivingDirectionsButton setCenter:CGPointMake(width/2, height*6/7 + 0.6*height/7)];
     [viewDrivingDirectionsButton addTarget:self action:@selector(logDrivingDirections) forControlEvents:UIControlEventTouchUpInside];
+    viewDrivingDirectionsButton.layer.cornerRadius = 8;
     [self.view addSubview:viewDrivingDirectionsButton];
 
 }
