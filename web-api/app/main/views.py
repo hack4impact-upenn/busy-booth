fc9533bab1cbfe89b461a8145d8baf377beea681
Cathy Chen <cathychenn@gmail.com>
2016-02-28 13:48:10 -0500

    basic endpoints

--- web-api/app/main/views.py
+++ web-api/app/main/views.py
@@ -1,27 +1,37 @@
 from . import main
-
 from flask.ext.login import login_required, current_user, login_user, logout_user
 from flask import request
 import re
 from ..models import User, WaitTime, PollingBooth
 import json
+import datetime
 
 
 @main.route('/')
 def index():
-    return "HELLO"
+    return "Running."
 
+
+# Testing URL - ignore
 @main.route('/loggedin')
-# @login_required
 def loggedin():
-	if current_user.is_authenticated():
-   		print current_user.id
-   	else:
-   		print "AAAAAA"
-	return "LOGGED IN"
+    if current_user.is_authenticated():
+        print current_user.id
+        return "Logged in."
+    else:
+        return "Not logged in."
 
-
+ 
 def phone_format(number):
+
+    """
+    Format an integer into a phone number.
+
+    Keyword arguments:
+    number -- 10 digit integer
+
+    """
+
     digits = re.sub(r'\D', '', number)
     if len(digits) == 10:
         digits = '1' + digits
@@ -29,78 +39,135 @@ def phone_format(number):
     return digits
 
 
-@main.route('/login', methods=['GET', 'POST'])
-def login():
-	print "in route"
-	if request.method == "POST":
+@main.route('/create_account', methods=['GET', 'POST'])
+def create_account():
 
+    """
+    Create new user.
 
-		phone = phone_format(request.form['phone'])
+    """
 
-		print phone
+    if request.method == "POST":
 
+        first_name = request.form['fname']
+        last_name = request.form['lname']
+        phone = phone_format(request.form['phone'])
+        address = request.form['address']
 
-		user = User.query.filter_by(phone=phone).first()
+        if User.query.filter_by(phone=phone).first() == None:
+            new_user = User(
+                first_name = first_name,
+                last_name = last_name,
+                phone = phone,
+                address = address
+                )
 
-		# if user is not None and user.verify_password(request.form['password']):
+            # How to set polling booth?
 
-		if user is not None:
-			login_user(user)
-			return "Logged in"
-
-	return "Failed logged in"
-
-
-@main.route('/logout', methods=['GET', 'POST'])
-@login_required
-def logout():
-	logout_user()
-	return True
-
-
-@main.route('/signup', methods=['GET', 'POST'])
-def signup():
-
-	print "SIGNING UP "
-
-	if request.method == "POST":
-		first_name = request.form['fname']
-    	last_name = request.form['lname']
-    	phone = phone_format(request.form['phone'])
-    	address = request.form['address']
-    	# polling_booth = 
-
-    	return True
-
-	return False
+            db.session.add(new_user)
+            db.session.commit()
+            return "Logged in %s." % first_name
 
 
 
-@main.route('/av_wait')
-def av_wait():
-	return "AV_WAIT"
+        else:
+            return "User exists."
 
 
-@main.route('/start_time', methods=['GET', 'POST'])
-def start_time():
-	wait_time = WaitTime(person=current_user)
-	db.session.add(wait_time)
-	db.session.commit()
-	return True
+@main.route('/av_wait/<int:booth_id>')
+def av_wait(booth_id):
+
+    """
+    Get average wait time for a polling booth.
+
+    Keyword arguments:
+    booth_id -- integer id of polling booth
+
+    """
+
+    polling_place = PollingBooth.query.filter_by(id=booth_id).first()
+
+    if polling_place:
+
+        past_hour = datetime.datetime.now() - datetime.timedelta(hours=1)
+
+        # get wait times in past hour
+        count = 0.0
+        elapsed_sum = 0
+        for time in polling_place.wait_times:
+            if time.start_time > past_hour and time.finished:
+                count += 1.0
+                elapsed_sum += time.elapsed
+
+        return json.dumps(elapsed_sum/count)
+
+    else:
+        return "Polling booth does not exist."
 
 
-@main.route('/end_time', methods=['GET', 'POST'])
-def end_time():
-	wait_time = current_user.waittime
-	wait_time.finished()
 
-	
+
+@main.route('/start_time/<phone>', methods=['GET', 'POST'])
+def start_time(phone):
+
+    """
+    Start wait time timer for user.
+
+    Keyword arguments:
+    phone -- phone number of user (10 digit)
+
+    """
+
+    user = User.query.filter_by(phone=phone_format(phone)).first()
+
+    if user == None:
+        return "User account does not exist"
+
+    if user.waittime == None:
+        wait_time = WaitTime()
+        user.waittime = wait_time
+        db.session.add(wait_time)
+        db.session.commit()
+        return "Wait time started."
+    else:
+        return "Wait time already exists."
+
+
+@main.route('/end_time/<phone>', methods=['GET', 'POST'])
+def end_time(phone):
+
+    """
+    End wait time timer for user.
+
+    Keyword arguments:
+    phone -- phone number of user (10 digit)
+
+    """
+
+    user = User.query.filter_by(phone=phone_format(phone)).first()
+
+    if user == None:
+        return "User account does not exist"
+
+    if user.waittime:
+        wait_time = user.waittime
+        wait_time.finished()
+        return "Wait time ended."
+    else:
+        return "Wait time doesn't exist."
+
 
 
 @main.route('/polling_places')
 def polling_places():
-	polling_places = PollingBooth.query.all()
-	return json.dumps([(x.name, [y.first_name for y in x.people]) for x in polling_places])
+
+    """
+    Get all polling places.
+
+    """
+    
+    polling_places = PollingBooth.query.all()
+    return json.dumps([(x.id, x.name, x.address, x.zip_code) for x in polling_places])
 
 
 
--- web-api/app/models/pollingbooth.py
+++ web-api/app/models/pollingbooth.py
@@ -8,13 +8,10 @@ class PollingBooth(db.Model):
     name = db.Column(db.String(64))
     address = db.Column(db.String(64))
     zip_code = db.Column(db.Integer)
-    avg_wait = db.Column(db.Integer)
     people = db.relationship('User', backref='pollingbooth', lazy='dynamic')
     wait_times = db.relationship('WaitTime', backref='pollingbooth', lazy='dynamic')
 
     def __init__(self, name, address, zip_code):
         self.name = name
         self.address = address
-        self.zip_code
-
-    # how to calculate average wait time?
+        self.zip_code = zip_code
