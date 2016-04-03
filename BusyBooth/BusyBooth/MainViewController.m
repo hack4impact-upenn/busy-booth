d06e5d37731c6ff33d76e5e369924f6721714ca0
Cathy Chen <cathychenn@gmail.com>
2016-04-03 14:49:02 -0400

    NSUserDefaults for currnet location

--- BusyBooth/BusyBooth/MainViewController.m
+++ BusyBooth/BusyBooth/MainViewController.m
@@ -10,7 +10,9 @@
 #import <MapKit/MapKit.h>
 #import "CheckIns.h"
 
-@interface MainViewController ()
+@interface MainViewController () <CLLocationManagerDelegate, MKMapViewDelegate>
+@property(nonatomic,strong) CLLocationManager *location;
+@property(nonatomic, strong) MKMapView *mapView;
 
 @end
 
@@ -43,23 +45,44 @@
     self.navigationItem.leftBarButtonItem.tintColor = [UIColor whiteColor];
     self.navigationController.navigationBar.barTintColor = mainColor;
     
-    MKMapView *mapView = [[MKMapView alloc] initWithFrame:self.view.frame];
-    [self.view addSubview:mapView];
-    
-    self.locationManager = [[CLLocationManager alloc] init];
-    self.locationManager.delegate = self;
-    
-    if ([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
-        [self.locationManager requestWhenInUseAuthorization];
-    }
-    
-    [self.locationManager startUpdatingLocation];
+    self.mapView = [[MKMapView alloc] initWithFrame:self.view.frame];
+    self.mapView.delegate = self;
     
     
-    mapView.showsUserLocation = YES;
     
+    self.location = [[CLLocationManager alloc]init];
+    self.location.delegate = self;
+    
+    [self.location requestAlwaysAuthorization];
+    [self.location startUpdatingLocation];
+    
+    self.mapView.showsUserLocation = YES;
+    
+    
+    [self.view addSubview:self.mapView];
 
-    [CheckIns checkingOutWithController:self];
+//    [CheckIns checkingOutWithController:self];
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    MKPinAnnotationView *pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:nil];
+    pin.enabled = YES;
+    pin.canShowCallout = YES;
+    return pin;
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    NSLog(@"Authorization Status changed.");
+}
+
+-(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
+    NSLog(@"Lat = %f, Lng = %f", userLocation.coordinate.latitude,userLocation.coordinate.longitude);
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setFloat:userLocation.coordinate.latitude forKey:@"latitude"];
+    [defaults setFloat:userLocation.coordinate.longitude forKey:@"longitude"];
+    [defaults synchronize];
+    [self.mapView setCenterCoordinate:userLocation.coordinate animated:YES];
 }
 
 
