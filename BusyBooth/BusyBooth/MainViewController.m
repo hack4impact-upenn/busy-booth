7d6e2601da7e76a26c0566b4114120b4eaf0e60e
Cathy Chen <cathychenn@gmail.com>
2016-04-17 15:35:29 -0400

    route overlay finished

--- BusyBooth/BusyBooth/MainViewController.m
+++ BusyBooth/BusyBooth/MainViewController.m
@@ -13,7 +13,9 @@
 @interface MainViewController () <CLLocationManagerDelegate, MKMapViewDelegate>
 @property(nonatomic,strong) CLLocationManager *location;
 @property(nonatomic, strong) MKMapView *mapView;
-
+@property(nonatomic, strong) MKPolyline *routeLine;
+@property(nonatomic, strong) MKPolylineView *lineView;
+@property(nonatomic, strong) MKRoute *routeDetails;
 @end
 
 @implementation MainViewController
@@ -25,6 +27,7 @@
     }
     return self;
 }
+
 
 -(void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
@@ -52,9 +55,12 @@
     [self.location requestAlwaysAuthorization];
     [self.location startUpdatingLocation];
     self.mapView.showsUserLocation = YES;
-    
-    
     [self.view addSubview:self.mapView];
+    
+    // hard code polling booth destination
+    CLLocation *dest = [[CLLocation alloc] initWithLatitude:39.949466 longitude:-75.171864];
+    [self drawRoute:[self.location location] goingTo:dest];
+    
 
 //    [CheckIns checkingOutWithController:self];
 }
@@ -78,6 +84,50 @@ didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
     [defaults setFloat:userLocation.coordinate.longitude forKey:@"longitude"];
     [defaults synchronize];
     [self.mapView setCenterCoordinate:userLocation.coordinate animated:YES];
+}
+
+-(void)drawRoute:(CLLocation *)source goingTo:(CLLocation *)destination {
+    
+    
+    MKDirectionsRequest *directionsRequest = [[MKDirectionsRequest alloc] init];
+    
+    
+    MKPlacemark *placemarkSource = [[MKPlacemark alloc] initWithCoordinate:CLLocationCoordinate2DMake(source.coordinate.latitude, source.coordinate.longitude) addressDictionary:nil];
+    MKPlacemark *placemarkDest = [[MKPlacemark alloc] initWithCoordinate:CLLocationCoordinate2DMake(destination.coordinate.latitude, destination.coordinate.longitude) addressDictionary:nil];
+    MKMapItem *sourceItem = [[MKMapItem alloc] initWithPlacemark:placemarkSource];
+    MKMapItem *destItem = [[MKMapItem alloc] initWithPlacemark:placemarkDest];
+    
+    [directionsRequest setSource:sourceItem];
+    [directionsRequest setDestination:destItem];
+    directionsRequest.transportType = MKDirectionsTransportTypeAutomobile;
+    MKDirections *directions = [[MKDirections alloc] initWithRequest:directionsRequest];
+    [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse *response, NSError *error) {
+        if (error) {
+            NSLog(@"Error %@", error.description);
+        } else {
+            
+            
+            self.routeDetails = response.routes.lastObject;
+            [self.mapView addOverlay:self.routeDetails.polyline];
+            NSLog(@"%@", [placemarkSource.addressDictionary objectForKey:@"Street"]);
+            NSLog(@"%@", [NSString stringWithFormat:@"%0.1f Miles", self.routeDetails.distance/1609.344]);
+            NSLog(@"%@", [NSString stringWithFormat:@"%lu" ,(unsigned long)self.routeDetails.transportType]);
+
+            for (int i = 0; i < self.routeDetails.steps.count; i++) {
+                MKRouteStep *step = [self.routeDetails.steps objectAtIndex:i];
+                NSString *newStep = step.instructions;
+                NSLog(@"%@", newStep);
+            }
+        }
+    }];
+
+}
+
+-(MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay {
+    MKPolylineRenderer  * routeLineRenderer = [[MKPolylineRenderer alloc] initWithPolyline:self.routeDetails.polyline];
+    routeLineRenderer.strokeColor = [UIColor redColor];
+    routeLineRenderer.lineWidth = 5;
+    return routeLineRenderer;
 }
 
 
